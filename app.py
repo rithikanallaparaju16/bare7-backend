9d21b13140264546f02c7ef80341a11aeeb83f28
Rithika <nrithikavarma@gmail.com>
2025-11-22 15:39:17 +0530

    Add DB port to connection

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import os
 
 app = Flask(__name__)
 
-def get_db():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT"))  # ✅ FIXED — added PORT
-    )
+connection = pymysql.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    port=int(os.getenv("DB_PORT")),   # <-- ADD THIS
+    database=os.getenv("DB_NAME")
+)
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
